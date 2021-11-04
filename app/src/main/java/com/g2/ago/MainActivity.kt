7710e7bcf8efbc822001d3881107bea983499ac3
mikel-ayala <75023127+mikel-ayala@users.noreply.github.com>
2021-11-04 12:01:04 +0100

    AGO project

---
+++ app/src/main/java/com/g2/ago/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.g2.ago
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
