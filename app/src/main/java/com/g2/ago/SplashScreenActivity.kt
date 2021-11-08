1f907b1eb1ac209edad88606905955ce4efab47f
GonzaloAlonso95 <gonzalo.alonso@elorrieta-errekamari.com>
2021-11-08 10:36:05 +0100

    Menu(strings)+Splash

---
+++ app/src/main/java/com/g2/ago/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.g2.ago
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+}
