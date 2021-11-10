63c30340e4950e10ccd197ce982e7ed14508c263
GonzaloAlonso95 <gonzalo.alonso@elorrieta-errekamari.com>
2021-11-10 09:30:03 +0100

    Activity Juegos

---
+++ app/src/main/java/com/g2/ago/JuegoActivity.kt
@@ -0,0 +1,11 @@
+package com.g2.ago
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JuegoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_juego)
+    }
+}
