780fbf70e59ac33e8209446db299dbcbec97fc1a
GonzaloAlonso95 <gonzalo.alonso@elorrieta-errekamari.com>
2021-12-02 12:41:58 +0100

    Update ModoLibre.kt

--- app/src/main/java/com/g2/ago/ModoLibre.kt
+++ app/src/main/java/com/g2/ago/ModoLibre.kt
@@ -7,7 +7,7 @@ class ModoLibre(context: Context)  {
     val PREFS_NAME = "com.g2.ago.sharedpreferences"
     val SHARED_NAME = "Modo"
     val name: SharedPreferences = context.getSharedPreferences(PREFS_NAME, 0)
-    var tipo: Boolean
+    var modo: Boolean
         //comprobamos el archivo de shared preferences
         get() = name.getBoolean(SHARED_NAME, false)
         //modificamos el valor de shared preferences
