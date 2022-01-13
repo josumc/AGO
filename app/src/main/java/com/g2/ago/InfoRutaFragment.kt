cc5776f3cbca46e5e978e31f21e7c078ad82a346
GonzaloAlonso95 <gonzalo.alonso@elorrieta-errekamari.com>
2022-01-13 13:28:35 +0100

    Textos de las paradas automáticos

--- app/src/main/java/com/g2/ago/InfoRutaFragment.kt
+++ app/src/main/java/com/g2/ago/InfoRutaFragment.kt
@@ -1,12 +1,11 @@
 package com.g2.ago
 
 import android.os.Bundle
-import android.os.SystemClock
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.g2.ago.databinding.FragmentInfoRutaBinding
 import kotlinx.android.synthetic.main.fragment_info_ruta.*
 
@@ -20,6 +19,17 @@ class InfoRutaFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentInfoRutaBinding.inflate(inflater, container, false)
+
+        //Genera el nombre del string que tiene que buscar
+        val parada ="parada"+ Sharedapp.puntopartida.Partida
+
+        //Usando la variable parada, busca el número de ID
+        val resID = resources.getIdentifier(parada,"string", activity!!.packageName)
+
+        //getString() recibe el ID que hemos recogido en la línea anterior y carga ese texto en el textView
+        binding.Ubicacion.text=getString(resID)
+
+        binding.EmpezarJuego.text=getString(R.string.Hasi)
         binding.EmpezarJuego.setOnClickListener{
             Toast.makeText(requireContext(), "${EmpezarJuego.isEnabled}", Toast.LENGTH_SHORT).show()
         }
